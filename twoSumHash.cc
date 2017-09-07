0f8eca5c1dfa152cc5b6a5a9487a5935b5c9e127
Yuheng Du <31047107+kevindo1119@users.noreply.github.com>
2017-09-07 13:41:00 -0400

    Using Hashing to solve two sum problem, runtime is O(n)

---
+++ twoSumHash.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+                // Use Hashing to reduce the runtime of picking int,
+                // Can use double hashing or Single Hashing
+                unordered_map<int, int> hashTable;
+                //init hashTable
+                for (int i = 0; i < nums.size(); ++i) {
+                        hashTable[nums.at(i)] = i;
+                }
+
+                for (int i = 0; i < nums.size(); ++i) {
+                        int rest = target - nums.at(i);
+
+                        if (hashTable.find(rest) != hashTable.end()){
+                                int index = hashTable.at(rest);
+                                if (index == i) {
+                                        continue;
+                                } else {
+                                        vector<int> result;
+                                        result.push_back(i+1);
+                                        result.push_back(nums.at(i+1));
+                                        return result;
+                                }
+                        }
+                }
+        }
+};
+
+// Test Driver
+int main() {
+
+        Solution sol;
+        vector<int> nums;
+        nums.push_back(2);
+        nums.push_back(3);
+        nums.push_back(4);
+        int target = 6;
+        vector<int> result = sol.twoSum(nums, target);
+        for (int i=0; i < result.size(); ++i) {
+                cout << result[i] << ' ';
+        }
+
+        return 0;
+}
