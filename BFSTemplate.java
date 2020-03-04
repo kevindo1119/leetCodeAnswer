6f3ae370f0d02abffebc1200b2e14477955cb124
Yuheng Du <31047107+kevindo1119@users.noreply.github.com>
2020-03-04 16:18:09 -0500

    Update BFSTemplate.java

--- BFSTemplate.java
+++ BFSTemplate.java
@@ -9,29 +9,36 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new List<>();
+        List<List<Integer>> res = new ArrayList<>();
         
-        if (root == null) {return res;}
+        if (root == null) {
+            return res;
+        }
         
         // 1. 创建一个队列，把起始节点都放进去
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
+        
         // 2. while 队列不空，处理队列中的节点，并拓展出新的节点
         while (!queue.isEmpty()) {
-            // 3. level x -> x + 1
             // for 上一层的节点拓展下一层的节点
             // queue.size() 写在for loop 里面的问题是， queue.size是改变的，不能完成层拓展，但是也没有问题。
+            List<Integer> level = new ArrayList<>();
             int size = queue.size();
-            for (int i = 0; i < size; ++i) { // level x
+            for (int i = 0; i < size; ++i) {
                 TreeNode node = queue.poll();
-                if (node.left != null) { // level x + 1
+                level.add(node.val);
+                if (node.left != null) {
                     queue.offer(node.left);
                 }
-                if (node.right  != null) { // level x + 1
+                if (node.right  != null) {
                     queue.offer(node.right);
                 }
             }
+            res.add(level);
         }
+        
+        return res;
     }
 }
+
